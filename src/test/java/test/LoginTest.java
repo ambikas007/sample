f28fee1c8b79a49c070cfa37826764780b6bda96
rajkshin <shinderaj161989@gmail.com>
2022-10-11 12:57:31 +0530

    added screenshots and log files

--- src/test/java/test/LoginTest.java
+++ src/test/java/test/LoginTest.java
@@ -14,18 +14,22 @@ public class LoginTest extends BaseClass {
 
         loginpage.setUserName(username);
         System.out.println("username is provided");
+        log.info("adding usernamae");
         Thread.sleep(5000);
         loginpage.setPassWord(password);
         System.out.println("password is provided");
+        log.info("adding password");
         loginpage.clickSubmit();
         System.out.println("clicked in submit button");
+        log.info("clicked submit");
 
         String url = driver.getCurrentUrl();
         System.out.println("navigated to homepage and url is: " + url);
 
-        if (url.contains("https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewEmployeeList")) {
+        if (url.contains("https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewEmployeeList00")) {
             Assert.assertTrue(true);
             System.out.println("login test passed");
+            log.info("logged in successfully");
 //            captureScreen(driver,"loginPassed");
         } else {
             Assert.assertTrue(false);
